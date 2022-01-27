8517a4101d1aacf3a1764d4bd4d5891531ea8ea6
Jenny Shen <jenny.shen@shopify.com>
2022-01-27 10:28:24 -0500

    Create and edit api keys scoped to a gem on the UI

--- test/integration/api_keys_test.rb
+++ test/integration/api_keys_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ApiKeysTest < SystemTest
   setup do
     @user = create(:user)
+    @ownership = create(:ownership, user: @user, rubygem: create(:rubygem))
 
     visit sign_in_path
     fill_in "Email or Username", with: @user.email
@@ -21,6 +22,20 @@ class ApiKeysTest < SystemTest
     assert page.has_content? "Note that we won't be able to show the key to you again. New API key:"
     assert @user.api_keys.last.can_index_rubygems?
     refute @user.api_keys.last.mfa_enabled?
+    refute @user.api_keys.last.rubygem
+  end
+
+  test "creating new api key scoped to a gem" do
+    visit_profile_api_keys_path
+
+    fill_in "api_key[name]", with: "test"
+    check "api_key[index_rubygems]"
+    assert page.has_select? "api_key_rubygem_id", selected: nil
+    page.select @ownership.rubygem.name
+    click_button "Create"
+
+    assert page.has_content? "Note that we won't be able to show the key to you again. New API key:"
+    assert_equal @ownership.rubygem, @user.api_keys.last.rubygem
   end
 
   test "creating new api key with MFA UI enabled" do
@@ -50,7 +65,7 @@ class ApiKeysTest < SystemTest
     assert @user.api_keys.last.mfa_enabled?
   end
 
-  test "update api key" do
+  test "update api key scope" do
     api_key = create(:api_key, user: @user)
 
     visit_profile_api_keys_path
@@ -62,6 +77,20 @@ class ApiKeysTest < SystemTest
     click_button "Update"
 
     assert api_key.reload.can_add_owner?
+  end
+
+  test "update api key gem scope" do
+    api_key = create(:api_key, user: @user, rubygem: @ownership.rubygem)
+
+    visit_profile_api_keys_path
+    click_button "Edit"
+
+    assert page.has_content? "Edit API key"
+    assert page.has_select? "api_key_rubygem_id", selected: @ownership.rubygem.name
+    page.select "All Gems"
+    click_button "Update"
+
+    assert_nil api_key.reload.rubygem
   end
 
   test "update api key with MFA UI enabled" do
@@ -114,6 +143,23 @@ class ApiKeysTest < SystemTest
     click_button "Reset"
 
     assert page.has_content? "New API key"
+  end
+
+  test "gem ownership removed displays api key as invalid" do
+    create(:api_key, user: @user, rubygem: @ownership.rubygem)
+    visit_profile_api_keys_path
+    refute page.has_css? ".owners__row__invalid"
+    refute page.has_content? "(ownership removed)"
+
+    @ownership.destroy!
+
+    visit_profile_api_keys_path
+    assert page.has_css? ".owners__row__invalid"
+    assert page.has_content? "(ownership removed)"
+
+    click_button "Edit"
+    assert page.has_select? "api_key_rubygem_id", selected: nil
+    assert page.has_css? ".flash"
   end
 
   def visit_profile_api_keys_path
