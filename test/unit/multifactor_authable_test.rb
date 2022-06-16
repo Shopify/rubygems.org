46cbb60150473a93e7aaa9d12369c049a5c85ddb
Jenny Shen <jenny.shen@shopify.com>
2022-06-16 17:09:04 -0400

    Refactor multifactorable tests to not reference user

--- test/unit/multifactor_authable_test.rb
+++ test/unit/multifactor_authable_test.rb
@@ -1,219 +1,223 @@
 require "test_helper"
 
 class MultifactorAuthableTest < ActiveSupport::TestCase
-  context "two factor authentication" do
-    setup do
-      @user = create(:user)
+  setup do
+    @multifactor_authable_class = User
+    @multifactor_authable = create(@multifactor_authable_class.name.underscore.to_sym)
+  end
+
+  context "#mfa_enabled" do
+    should "return true if multifactor auth is not disabled" do
+      @multifactor_authable.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
+      assert_predicate @multifactor_authable, :mfa_enabled?
     end
 
-    should "disable mfa by default" do
-      refute_predicate @user, :mfa_enabled?
-    end
-
-    context "when enabled" do
-      setup do
-        @user.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
-      end
-
-      should "be able to use a recovery code only once" do
-        code = @user.mfa_recovery_codes.first
-        assert @user.otp_verified?(code)
-        refute @user.otp_verified?(code)
-      end
-
-      should "be able to verify correct OTP" do
-        assert @user.otp_verified?(ROTP::TOTP.new(@user.mfa_seed).now)
-      end
-
-      should "return true for mfa status check" do
-        assert_predicate @user, :mfa_enabled?
-        refute_predicate @user, :mfa_disabled?
-      end
-
-      should "return true for otp in last interval" do
-        last_otp = ROTP::TOTP.new(@user.mfa_seed).at(Time.current - 30)
-        assert @user.otp_verified?(last_otp)
-      end
-
-      should "return true for otp in next interval" do
-        next_otp = ROTP::TOTP.new(@user.mfa_seed).at(Time.current + 30)
-        assert @user.otp_verified?(next_otp)
-      end
-
-      context "blocking user with api key" do
-        setup { create(:api_key, user: @user) }
-
-        should "reset email and mfa" do
-          assert_changed(@user, :email, :password, :api_key, :mfa_seed, :remember_token) do
-            @user.block!
-          end
-
-          assert @user.email.start_with?("security+locked-")
-          assert @user.email.end_with?("@rubygems.org")
-          assert_empty @user.mfa_recovery_codes
-          assert_predicate @user, :mfa_disabled?
-        end
-
-        should "reset api key" do
-          @user.block!
-          assert_nil @user.api_key
-          assert_empty @user.api_keys
-        end
-      end
-    end
-
-    context "when disabled" do
-      setup do
-        @user.disable_mfa!
-      end
-
-      should "return false for verifying OTP" do
-        refute @user.otp_verified?("")
-      end
-
-      should "return false for mfa status check" do
-        refute_predicate @user, :mfa_enabled?
-        assert_predicate @user, :mfa_disabled?
-      end
+    should "return true if multifactor auth is disabled" do
+      @multifactor_authable.disable_mfa!
+      refute_predicate @multifactor_authable, :mfa_enabled?
     end
   end
 
-  context "strong_mfa_level?" do
-    should "be true if the users mfa level is ui_and_api" do
-      user = create(:user, mfa_level: "ui_and_api")
-
-      assert_predicate user, :strong_mfa_level?
+  context "#disable_mfa!" do
+    setup do
+      @multifactor_authable.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
+      @multifactor_authable.disable_mfa!
     end
 
-    should "be true if the users mfa level is ui_and_gem_signin" do
-      user = create(:user, mfa_level: "ui_and_gem_signin")
-
-      assert_predicate user, :strong_mfa_level?
-    end
-
-    should "be false if users mfa level is ui_only" do
-      user = create(:user, mfa_level: "ui_only")
-
-      refute_predicate user, :strong_mfa_level?
-    end
-
-    should "be false if users has mfa disabled" do
-      user = create(:user, mfa_level: "disabled")
-
-      refute_predicate user, :strong_mfa_level?
+    should "disable mfa" do
+      assert_predicate @multifactor_authable, :mfa_disabled?
+      assert_empty @multifactor_authable.mfa_seed
+      assert_empty @multifactor_authable.mfa_recovery_codes
     end
   end
 
-  context "recommend mfa" do
+  context "#verify_and_enable_mfa!" do
     setup do
-      @user = create(:user)
-      @rubygem = create(:rubygem)
-      create(:ownership, user: @user, rubygem: @rubygem)
-      assert_equal [@rubygem], @user.rubygems
+      @seed = ROTP::Base32.random_base32
+      @expiry = 30.minutes.from_now
     end
 
-    context "when a user doesn't own a gem with more downloads than the recommended threshold" do
-      setup do
-        GemDownload.increment(
-          Rubygem::MFA_RECOMMENDED_THRESHOLD,
-          rubygem_id: @rubygem.id
-        )
-      end
+    should "enable mfa" do
+      @multifactor_authable.verify_and_enable_mfa!(
+        @seed,
+        :ui_and_api,
+        ROTP::TOTP.new(@seed).now,
+        @expiry
+      )
 
-      should "return false for mfa_recommended?" do
-        refute_predicate @user, :mfa_recommended?
-      end
-
-      should "return false for mfa_recommended_not_yet_enabled?" do
-        refute_predicate @user, :mfa_recommended_not_yet_enabled?
-      end
-
-      should "return false for mfa_recommended_weak_level_enabled?" do
-        refute_predicate @user, :mfa_recommended_weak_level_enabled?
-      end
+      assert_predicate @multifactor_authable, :mfa_enabled?
     end
 
-    context "when mfa disabled user owns a gem with more downloads than the recommended threshold" do
-      setup do
-        GemDownload.increment(
-          Rubygem::MFA_RECOMMENDED_THRESHOLD + 1,
-          rubygem_id: @rubygem.id
-        )
-      end
+    should "add error if qr code expired" do
+      @multifactor_authable.verify_and_enable_mfa!(
+        @seed,
+        :ui_and_api,
+        ROTP::TOTP.new(@seed).now,
+        5.minutes.ago
+      )
 
-      should "return true for mfa_recommended?" do
-        assert_predicate @user, :mfa_recommended?
-      end
-
-      should "return true for mfa_recommended_not_yet_enabled?" do
-        assert_predicate @user, :mfa_recommended_not_yet_enabled?
-      end
-
-      should "return false for mfa_recommended_weak_level_enabled?" do
-        refute_predicate @user, :mfa_recommended_weak_level_enabled?
-      end
+      refute_predicate @multifactor_authable, :mfa_enabled?
+      expected_error = "The QR-code and key is expired. Please try registering a new device again."
+      assert_contains @multifactor_authable.errors[:base], expected_error
     end
 
-    context "when mfa `ui_only` user owns a gem with more downloads than the recommended threshold" do
-      setup do
-        @user.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
+    should "add error if otp code is incorrect" do
+      @multifactor_authable.verify_and_enable_mfa!(
+        @seed,
+        :ui_and_api,
+        ROTP::TOTP.new(ROTP::Base32.random_base32).now,
+        @expiry
+      )
 
-        GemDownload.increment(
-          Rubygem::MFA_RECOMMENDED_THRESHOLD + 1,
-          rubygem_id: @rubygem.id
-        )
-      end
-
-      should "return true for mfa_recommended?" do
-        assert_predicate @user, :mfa_recommended?
-      end
-
-      should "return false for mfa_recommended_not_yet_enabled?" do
-        refute_predicate @user, :mfa_recommended_not_yet_enabled?
-      end
-
-      should "return true for mfa_recommended_weak_level_enabled?" do
-        assert_predicate @user, :mfa_recommended_weak_level_enabled?
-      end
+      refute_predicate @multifactor_authable, :mfa_enabled?
+      assert_contains @multifactor_authable.errors[:base], "Your OTP code is incorrect."
     end
-    
+  end
 
+  context "#enable_mfa!" do
+    setup do
+      @seed = ROTP::Base32.random_base32
+      @level = :ui_and_api
+      @multifactor_authable.enable_mfa!(@seed, @level)
+    end
 
-    context "when strong user owns a gem with more downloads than the recommended threshold" do
-      setup do
-        @user.enable_mfa!(ROTP::Base32.random_base32, :ui_and_api)
+    should "enable mfa" do
+      assert_equal @seed, @multifactor_authable.mfa_seed
+      assert_predicate @multifactor_authable, :mfa_ui_and_api?
+      assert_equal 10, @multifactor_authable.mfa_recovery_codes.length
+    end
+  end
 
-        GemDownload.increment(
-          Rubygem::MFA_RECOMMENDED_THRESHOLD + 1,
-          rubygem_id: @rubygem.id
-        )
-      end
+  context "#mfa_gem_signin_authorized?" do
+    setup do
+      @seed = ROTP::Base32.random_base32
+    end
 
-      should "return false for mfa_recommended?" do
-        refute_predicate @user, :mfa_recommended?
-      end
+    should "return true if mfa is ui_and_api and otp is correct" do
+      @multifactor_authable.enable_mfa!(@seed, :ui_and_api)
+      assert @multifactor_authable.mfa_gem_signin_authorized?(ROTP::TOTP.new(@seed).now)
+    end
 
-      should "return false for mfa_recommended_not_yet_enabled?" do
-        refute_predicate @user, :mfa_recommended_not_yet_enabled?
-      end
+    should "return true if mfa is ui_and_gem_signin and otp is correct" do
+      @multifactor_authable.enable_mfa!(@seed, :ui_and_gem_signin)
+      assert @multifactor_authable.mfa_gem_signin_authorized?(ROTP::TOTP.new(@seed).now)
+    end
 
-      should "return false for mfa_recommended_weak_level_enabled?" do
-        refute_predicate @user, :mfa_recommended_weak_level_enabled?
-      end
+    should "return true if mfa is disabled" do
+      assert @multifactor_authable.mfa_gem_signin_authorized?(ROTP::TOTP.new(@seed).now)
+    end
+
+    should "return true if mfa is ui_only" do
+      @multifactor_authable.enable_mfa!(@seed, :ui_only)
+      assert @multifactor_authable.mfa_gem_signin_authorized?(ROTP::TOTP.new(@seed).now)
+    end
+
+    should "return false if otp is incorrect" do
+      @multifactor_authable.enable_mfa!(@seed, :ui_and_gem_signin)
+      refute @multifactor_authable.mfa_gem_signin_authorized?(ROTP::TOTP.new(ROTP::Base32.random_base32).now)
+    end
+  end
+
+  context "#mfa_recommended_not_yet_enabled?" do
+    setup do
+      @popular_rubygem = create(:rubygem)
+      GemDownload.increment(
+        Rubygem::MFA_RECOMMENDED_THRESHOLD + 1,
+        rubygem_id: @popular_rubygem.id
+      )
+    end
+
+    should "return true if instance owns a gem that exceeds recommended threshold and has mfa disabled" do
+      create(:ownership, user: @multifactor_authable, rubygem: @popular_rubygem)
+
+      assert_predicate @multifactor_authable, :mfa_recommended_not_yet_enabled?
+    end
+
+    should "return false if instance owns a gem that exceeds recommended threshold and has mfa enabled" do
+      create(:ownership, user: @multifactor_authable, rubygem: @popular_rubygem)
+      @multifactor_authable.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
+
+      refute_predicate @multifactor_authable, :mfa_recommended_not_yet_enabled?
+    end
+
+    should "return false if instance does not own a gem that exceeds recommended threshold and has mfa disabled" do
+      create(:ownership, user: @multifactor_authable, rubygem: create(:rubygem))
+
+      refute_predicate @multifactor_authable, :mfa_recommended_not_yet_enabled?
+    end
+  end
+
+  context "#mfa_recommended_weak_level_enabled?" do
+    setup do
+      @popular_rubygem = create(:rubygem)
+      GemDownload.increment(
+        Rubygem::MFA_RECOMMENDED_THRESHOLD + 1,
+        rubygem_id: @popular_rubygem.id
+      )
+      @multifactor_authable.enable_mfa!(ROTP::Base32.random_base32, :ui_only)
+    end
+
+    should "return true if instance owns a gem that exceeds recommended threshold and has mfa ui_only" do
+      create(:ownership, user: @multifactor_authable, rubygem: @popular_rubygem)
+
+      assert_predicate @multifactor_authable, :mfa_recommended_weak_level_enabled?
+    end
+
+    should "return false if instance owns a gem that exceeds recommended threshold and has mfa disabled" do
+      create(:ownership, user: @multifactor_authable, rubygem: @popular_rubygem)
+      @multifactor_authable.disable_mfa!
+
+      refute_predicate @multifactor_authable, :mfa_recommended_weak_level_enabled?
+    end
+
+    should "return false if instance does not own a gem that exceeds recommended threshold and has mfa disabled" do
+      create(:ownership, user: @multifactor_authable, rubygem: create(:rubygem))
+
+      refute_predicate @multifactor_authable, :mfa_recommended_weak_level_enabled?
+    end
+  end
+
+  context "#otp_verified?" do
+    setup do
+      @multifactor_authable.enable_mfa!(ROTP::Base32.random_base32, :ui_and_api)
+    end
+
+    should "return true if otp is correct" do
+      assert @multifactor_authable.otp_verified?(ROTP::TOTP.new(@multifactor_authable.mfa_seed).now)
+    end
+
+    should "return true for otp in last interval" do
+      last_otp = ROTP::TOTP.new(@multifactor_authable.mfa_seed).at(Time.current - 30)
+      assert @multifactor_authable.otp_verified?(last_otp)
+    end
+
+    should "return true for otp in next interval" do
+      next_otp = ROTP::TOTP.new(@multifactor_authable.mfa_seed).at(Time.current + 30)
+      assert @multifactor_authable.otp_verified?(next_otp)
+    end
+
+    should "return false if otp is incorrect" do
+      refute @multifactor_authable.otp_verified?(ROTP::TOTP.new(ROTP::Base32.random_base32).now)
+    end
+
+    should "return true if recovery code is correct" do
+      recovery_code = @multifactor_authable.mfa_recovery_codes.first
+
+      assert @multifactor_authable.otp_verified?(recovery_code)
+      refute_includes @multifactor_authable.mfa_recovery_codes, recovery_code
     end
   end
 
   context ".without_mfa" do
     setup do
-      create(:user, handle: "has_mfa", mfa_level: "ui_and_api")
-      create(:user, handle: "no_mfa", mfa_level: "disabled")
+      create(@multifactor_authable_class.name.underscore.to_sym, mfa_level: :ui_and_api)
     end
 
-    should "return only users without mfa" do
-      users_without_mfa = User.without_mfa
+    should "return instances without mfa" do
+      multifactor_authable_without_mfa = @multifactor_authable_class.without_mfa
 
-      assert_equal 1, users_without_mfa.size
-      assert_equal "no_mfa", users_without_mfa.first.handle
+      assert_equal 1, multifactor_authable_without_mfa.size
+      assert_equal @multifactor_authable, multifactor_authable_without_mfa.first
     end
   end
 end
