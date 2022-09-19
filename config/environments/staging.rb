b014e00bb5f332a6bd284374e628fbc9d0d38b7f
Ashley Ellis Pierce <anellis12@gmail.com>
2022-09-19 18:03:49 -0400

    Remove config changes
    I believe these changes were introduced to remove stock comments,
    however since this feature branch is
    very large, I'd like to decouple it from unrelated refactorings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,11 +30,12 @@ Rails.application.configure do
     'Expires' => 'Thu, 31 Dec 2037 23:55:55 GMT'
   }
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
