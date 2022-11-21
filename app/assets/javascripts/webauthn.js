5ddc7ae189dd9fa25fda1aee6f9d2162252d40eb
Ashley Ellis Pierce <anellis12@gmail.com>
2022-11-21 16:14:18 -0500

    Clean up javascript

--- app/assets/javascripts/webauthn.js
+++ app/assets/javascripts/webauthn.js
@@ -1,26 +1,26 @@
 (function() {
   var handleEvent = function(event) {
-    event.preventDefault()
-    return event.target
-  }
+    event.preventDefault();
+    return event.target;
+  };
 
   var setError = function(submit, error, exception) {
-    submit.attr("disabled", false)
-    error.attr("hidden", false)
-    error.text(exception.message)
-  }
+    submit.attr("disabled", false);
+    error.attr("hidden", false);
+    error.text(exception.message);
+  };
 
   var handleResponse = function(submit, responseError, response) {
     if (response.redirected) {
-      window.location.href = response.url
+      window.location.href = response.url;
     } else {
       response.json().then(function (json) {
-        setError(submit, error, json.message)
+        setError(submit, responseError, json.message);
       }).catch(function (error) {
-        setError(submit, responseError, error)
-      })
+        setError(submit, responseError, error);
+      });
     }
-  }
+  };
 
   var credentialsToBase64 = function(credentials) {
     return {
@@ -33,42 +33,42 @@
         attestationObject: bufferToBase64url(credentials.response.attestationObject),
         clientDataJSON: bufferToBase64url(credentials.response.clientDataJSON),
         signature: bufferToBase64url(credentials.response.signature)
-      }
-    }
-  }
+      },
+    };
+  };
 
   var credentialsToBuffer = function(credentials) {
     return credentials.map(function(credential) {
       return {
         id: base64urlToBuffer(credential.id),
         type: credential.type
-      }
-    })
-  }
+      };
+    });
+  };
 
   $(function() {
-    var credentialForm = $(".js-new-webauthn-credential--form")
-    var credentialError = $(".js-new-webauthn-credential--error")
-    var credentialSubmit = $(".js-new-webauthn-credential--submit")
-    var csrfToken = $("[name='csrf-token']").attr("content")
+    var credentialForm = $(".js-new-webauthn-credential--form");
+    var credentialError = $(".js-new-webauthn-credential--error");
+    var credentialSubmit = $(".js-new-webauthn-credential--submit");
+    var csrfToken = $("[name='csrf-token']").attr("content");
 
     credentialForm.submit(function(event) {
-      var form = handleEvent(event)
-      var nickname = $(".js-new-webauthn-credential--nickname").val()
+      var form = handleEvent(event);
+      var nickname = $(".js-new-webauthn-credential--nickname").val();
 
       fetch(form.action + ".json", {
         method: "POST",
         credentials: "same-origin",
         headers: { "X-CSRF-Token": csrfToken }
       }).then(function (response) {
-        return response.json()
+        return response.json();
       }).then(function (json) {
-        json.user.id = base64urlToBuffer(json.user.id)
-        json.challenge = base64urlToBuffer(json.challenge)
-        json.excludeCredentials = credentialsToBuffer(json.excludeCredentials)
+        json.user.id = base64urlToBuffer(json.user.id);
+        json.challenge = base64urlToBuffer(json.challenge);
+        json.excludeCredentials = credentialsToBuffer(json.excludeCredentials);
         return navigator.credentials.create({
           publicKey: json
-        })
+        });
       }).then(function (credentials) {
         return fetch(form.action + "/callback.json", {
           method: "POST",
@@ -81,27 +81,26 @@
             credentials: credentialsToBase64(credentials),
             webauthn_credential: { nickname: nickname }
           })
-        })
+        });
       }).then(function (response) {
-        handleResponse(credentialSubmit, credentialError, response)
+        handleResponse(credentialSubmit, credentialError, response);
       }).catch(function (error) {
-        setError(credentialSubmit, credentialError, error)
-      })
-    })
-  })
+        setError(credentialSubmit, credentialError, error);
+      });
+    });
+  });
 
   $(function() {
-    var sessionForm = $(".js-webauthn-session--form")
-    var sessionSubmit = $(".js-webauthn-session--submit")
-    var sessionError = $(".js-webauthn-session--error")
-    var csrfToken = $("[name='csrf-token']").attr("content")
+    var sessionForm = $(".js-webauthn-session--form");
+    var sessionSubmit = $(".js-webauthn-session--submit");
+    var sessionError = $(".js-webauthn-session--error");
+    var csrfToken = $("[name='csrf-token']").attr("content");
 
-    sessionForm.submit(function(e) {
-      var form = handleEvent(event)
-      var options = JSON.parse(form.dataset.options)
-      options.challenge = base64urlToBuffer(options.challenge)
-      options.userVerification = "preferred"
-      options.allowCredentials = credentialsToBuffer(options.allowCredentials)
+    sessionForm.submit(function(event) {
+      var form = handleEvent(event);
+      var options = JSON.parse(form.dataset.options);
+      options.challenge = base64urlToBuffer(options.challenge);
+      options.allowCredentials = credentialsToBuffer(options.allowCredentials);
       navigator.credentials.get({
         publicKey: options
       }).then(function (credentials) {
@@ -115,12 +114,12 @@
           body: JSON.stringify({
             credentials: credentialsToBase64(credentials)
           })
-        })
+        });
       }).then(function (response) {
-        handleResponse(sessionSubmit, sessionError, response)
+        handleResponse(sessionSubmit, sessionError, response);
       }).catch(function (error) {
-        setError(sessionSubmit, sessionError, error)
-      })
-    })
-  })
-})()
+        setError(sessionSubmit, sessionError, error);
+      });
+    });
+  });
+})();
