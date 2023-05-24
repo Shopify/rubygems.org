a85df55130ba6f3704b8717e9dc5b235748e3f6d
Jenny Shen <jenny.shen@shopify.com>
2023-05-24 13:29:00 -0400

    Change mfa_update and webauthn_update to PUT requests

--- config/routes.rb
+++ config/routes.rb
@@ -138,8 +138,8 @@ Rails.application.routes.draw do
     resource :dashboard, only: :show, constraints: { format: /html|atom/ }
     resources :profiles, only: :show
     resource :multifactor_auth, only: %i[new create update destroy] do
-      post 'mfa_update', to: 'multifactor_auths#mfa_update', as: :mfa_update
-      post 'webauthn_update', to: 'multifactor_auths#webauthn_update', as: :webauthn_update
+      put 'mfa_update', to: 'multifactor_auths#mfa_update', as: :mfa_update
+      put 'webauthn_update', to: 'multifactor_auths#webauthn_update', as: :webauthn_update
     end
     resource :settings, only: :edit
     resource :profile, only: %i[edit update] do
